3441521e22534c64a051255cecfa2259d10d1b3b
MsDev18 <m.sadegh0086@gmail.com>
2026-07-08 18:23:21 +0330

    implement reponse package

---
+++ internal/pkg/response/response.go
@@ -0,0 +1,40 @@
+package response
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Responder struct {
+	ctx *gin.Context
+}
+
+type body struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+	Data    any    `json:"data,omitempty"`
+}
+
+func New(ctx *gin.Context) Responder {
+	return Responder{
+		ctx: ctx,
+	}
+}
+
+func (r Responder) Send(code int, message string, data any) {
+	r.ctx.JSON(code, body{
+		Code:    code,
+		Message: message,
+		Data:    data,
+	})
+}
+
+// helper
+func (r Responder) OK(message string, data any) {
+	r.Send(http.StatusOK, message, data)
+}
+// helper
+func (r Responder) Created(message string, data any) {
+	r.Send(http.StatusCreated, message, data)
+}
