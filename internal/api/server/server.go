ad89bdcead808b38ffdd895fa18a74af80d8772a
MsDev18 <m.sadegh0086@gmail.com>
2026-07-24 23:16:20 +0330

    read data from config (change type uint to time.Duration

--- internal/api/server/server.go
+++ internal/api/server/server.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"shop/internal/api/handler/auth"
+	"shop/internal/api/handler/health"
 	"shop/internal/api/router"
 	"shop/internal/pkg/richerror"
 	"time"
@@ -14,20 +16,18 @@ import (
 type Server struct {
 	config     Config
 	httpServer *http.Server
-	// handler statements
-	handlers []router.RegisterRoutes
 }
 
 type Config struct {
-	Host         string `koanf:"host"`
-	Port         uint   `koanf:"port"`
-	WriteTimeout uint   `koanf:"write_timeout"`
-	ReadTimeout  uint   `koanf:"read_timeout"`
-	IdleTimeout  uint   `koanf:"idle_timeout"`
-	Env          string `koanf:"env"`
+	Host         string        `koanf:"host"`
+	Port         uint          `koanf:"port"`
+	WriteTimeout time.Duration `koanf:"write_timeout"`
+	ReadTimeout  time.Duration `koanf:"read_timeout"`
+	IdleTimeout  time.Duration `koanf:"idle_timeout"`
+	Env          string        `koanf:"env"`
 }
 
-func New(config Config, handlers ...router.RegisterRoutes) Server {
+func New(config Config, healthHandler health.Handler, authHandler auth.Handler) Server {
 	// validation env
 	env := Env(config.Env)
 	if !env.IsValid() {
@@ -37,7 +37,7 @@ func New(config Config, handlers ...router.RegisterRoutes) Server {
 	if config.ReadTimeout == 0 || config.WriteTimeout == 0 || config.IdleTimeout == 0 {
 		log.Fatalf("Invalid timeout values: read_timeout=%d, write_timeout=%d, idle_timeout=%d", config.ReadTimeout, config.WriteTimeout, config.IdleTimeout)
 	}
-	
+
 	// set from gin mode based on env
 	if env == EnvDevelopment {
 		gin.SetMode(gin.DebugMode)
@@ -47,23 +47,21 @@ func New(config Config, handlers ...router.RegisterRoutes) Server {
 	// create gin engine
 	engine := gin.Default()
 	// register routes
-	appRouter := router.New(engine, handlers...)
+	appRouter := router.New(engine, healthHandler, authHandler)
 	appRouter.Register()
 
 	// manually create http server to set timeouts
 	httpServer := &http.Server{
 		Addr:         fmt.Sprintf("%s:%d", config.Host, config.Port),
 		Handler:      engine,
-		ReadTimeout:  time.Duration(config.ReadTimeout) * time.Second,
-		WriteTimeout: time.Duration(config.WriteTimeout) * time.Second,
-		IdleTimeout:  time.Duration(config.IdleTimeout) * time.Second,
+		ReadTimeout:  config.ReadTimeout,
+		WriteTimeout: config.WriteTimeout,
+		IdleTimeout:  config.IdleTimeout,
 	}
 
 	return Server{
 		config:     config,
 		httpServer: httpServer,
-		// handlers statements
-		handlers: handlers,
 	}
 }
 
