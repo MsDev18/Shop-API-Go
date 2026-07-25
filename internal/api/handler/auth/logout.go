1bdcd97582966c9d273ca23353ce069b0b1a516c
MsDev18 <m.sadegh0086@gmail.com>
2026-07-26 00:43:07 +0330

    implement logout enpoint

---
+++ internal/api/handler/auth/logout.go
@@ -0,0 +1,43 @@
+package auth
+
+import (
+	"shop/internal/api/middleware"
+	"shop/internal/pkg/response"
+	"shop/internal/pkg/richerror"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (h Handler) Logout(ctx *gin.Context) {
+	const op = "auth-handler.Logout"
+
+	value, exists := ctx.Get(middleware.SESSION_ID_KEY)
+	if !exists {
+		response.New(ctx).Error(
+			richerror.New().
+				SetOp(op).
+				SetMsg("session id not found in context").
+				SetKind(richerror.KindUnexpectedErr),
+		)
+		return
+	}
+
+	sessionID, ok := value.(uint)
+	if !ok {
+		response.New(ctx).Error(
+			richerror.New().
+				SetOp(op).
+				SetMsg("invalid session id type in context").
+				SetKind(richerror.KindUnexpectedErr),
+		)
+		return
+	}
+
+	if err := h.service.Logout(ctx , sessionID) ; err != nil {
+		response.New(ctx).Error(err)
+		return
+	}
+
+	ctx.SetCookie("refresh-token" , "" , -1, "/", "" ,true, true)
+	response.New(ctx).OK("logout successfully" , nil)
+}
