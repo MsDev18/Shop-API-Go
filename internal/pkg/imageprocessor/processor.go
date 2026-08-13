e2d92add29b0c22781ed458081281bd66366c341
MsDev18 <m.sadegh0086@gmail.com>
2026-08-13 16:43:21 +0330

    better structure

--- internal/pkg/imageprocessor/processor.go
+++ internal/pkg/imageprocessor/processor.go
@@ -1,8 +1,17 @@
 package imageprocessor
 
+import (
+	"context"
+	"io"
+)
+
 type Processor struct {
 	config Config
 	storage Storage
+}
+
+type Storage interface {
+	Save(ctx context.Context , filename string, data io.Reader) (string , error)
 }
 
 func New (config Config , storage Storage) Processor {
